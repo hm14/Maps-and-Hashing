5340886721a37d5073a8e16a4a1c5fa138c56aaa
Hannanah Mobashir <hanana14@yahoo.com>
2017-08-29 14:47:45 -0400

    first commit

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+# create empty dictionary
+my_dictionary = {}
+# fill dictionary step by step
+first_dictionary['a'] = 'Apple'
+my_dictionary['b'] = 'Ball'
+my_dictionary['c'] = 'Cat'
+
+print my_dictionary
+print my_dictionary['c']
+
+# create dictionary at once
+another_dictionary = {'a': 'Alpha', 'b': 'Bravo', 'c': 'Charlie'}
+print another_dictionary
+print another_dictionary['c']
